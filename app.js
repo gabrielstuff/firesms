9edbc648fb9cfa380f40cd13382d1b384e7cebfc
gabrielstuff <gabriel@soixantecircuits.fr>
2017-09-11 23:04:56 +0200

    add firebase and standard-settings

--- app.js
+++ app.js
@@ -2,15 +2,53 @@
 let port = 3333
 var express = require('express')
 var multer  = require('multer')
+const settings = require('standard-settings').getSettings()
 var arrayParser = multer().array()
-var essendexHandler = require('./post/essendex').push
+var essendexHandler = require('./post/esendex').push
 var app = express()
+
+var admin = require("firebase-admin")
+
+var serviceAccount = require(settings.service.firebase.key.path)
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${settings.service.firebase.database.name}.firebaseio.com` 
+})
+
+
+function writeNewSMS(uid, body, author) {
+  // A post entry.
+  var postData = {
+    uid: uid,
+    body: body,
+    author: author,
+    createdAt: admin.database.ServerValue.TIMESTAMP,
+    display: true
+  }
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('sms').push().key
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {}
+  updates['/sms/' + newPostKey] = postData
+  return firebase.database().ref().update(updates)
+}
 
 app.get('/', function (req, res) {
   res.send('Hello SMS!')
 });
 
-app.post('/sms', arrayParser, essendexHandler)
+app.post('/sms', arrayParser, (req, res)=>{
+  essendexHandler(req, res, (error, message)=>{
+    if(error){
+      console.error(error)
+    } else {
+      writeNewSMS(message.uid, message.body, message.author)
+    }
+  })
+})
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
